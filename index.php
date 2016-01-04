de45fc20a40ea96c6db5028d3c0faf1a79063e8a
RealPolluX <jawi785@googlemail.com>
2016-01-04 04:13:36 +0100

    example configuration
    Added example.ini
    deleted test lines in index.php

--- index.php
+++ index.php
@@ -15,26 +15,9 @@ session_start();
 require_once('vendor/autoload.php');
 
 $core = new \TeamManager\Core();
-echo "<pre>";
-print_r($core->getSettings());
-echo "</pre>";
-
-echo "<br><br><br>";
-
-$a = array('a','hure','hurensohn','deine mama kackt','deine mama kackt einen','deine mama kackt einen hurensohn','deine mama kackt einen hurensohn zum','deine mama kackt einen hurensohn zum mittag');
-foreach($a as $b){
-    $hash = $core->createHash($b, 'bcrypt');
-    echo $hash;
-    echo ' | lenght: '.strlen($hash).'<br/>';
-}
+$core->execute();
 
 // TODO: password_hash & password_verify für login
 // TODO: registration mit key, den nur der admin einsehen kann + recaptcha
 
-echo "<br><br><br><br><br>";
-echo password_verify('a', '$2y$10$/SZHLP8WtxCx6ezoh99ZQOcsZuNcskSPJ0Vu9iaRsXQ5I9n82Ft7u');
-echo password_verify('a', '$2y$10$RwdCngh85ZHHAWtJpgEg5eNhofeDjdwxj66MO5.loZsGeupkq3Wi2');
-?>
 
-a -> $2y$10$/SZHLP8WtxCx6ezoh99ZQOcsZuNcskSPJ0Vu9iaRsXQ5I9n82Ft7u
-a -> $2y$10$RwdCngh85ZHHAWtJpgEg5eNhofeDjdwxj66MO5.loZsGeupkq3Wi2
